a3ea3257c4cc59b893edfb7fa4abead287fe6548
Greg Morgan <gregm9999@gmail.com>
2018-11-24 04:13:26 +0000

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,32 @@
+require"pry"
+class Show < ActiveRecord::Base
+
+  def Show::highest_rating
+    Show.maximum("rating")
+  end
+
+  def Show::most_popular_show
+    Show.where(rating: highest_rating)[0]
+  end
+
+  def Show::lowest_rating
+    Show.minimum("rating")
+  end
+
+  def Show::least_popular_show
+    Show.order(rating: :desc).last
+  end
+
+  def Show::ratings_sum
+    Show.sum("rating")
+  end
+
+  def Show::popular_shows
+    Show.where("rating > 5")
+  end
+
+  def Show::shows_by_alphabetical_order
+    Show.order(name: :asc)
+  end
+
+end
