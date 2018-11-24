de2b46e76e4f80d181ba1e03341fff1cd9e03d5b
Greg Morgan <gregm9999@gmail.com>
2018-11-24 04:17:50 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,4 @@
+
 require"pry"
 class Show < ActiveRecord::Base
 
--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -2,4 +2,4 @@ class AddSeasonToShows < ActiveRecord::Migration
    def change
     add_column :shows, :season, :string
   end
- end 
+ end
